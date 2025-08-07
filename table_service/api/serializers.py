4b2f24b64d053f9207e0402638b26c1e4c39c260
Evgeny1337 <genya-sozykin@mal.ru>
2025-08-07 17:09:48 +0500

    Исправил вывод создателя таблицы

--- table_service/api/serializers.py
+++ table_service/api/serializers.py
@@ -72,14 +72,12 @@ class ProfileCreateUpdateSerializer(serializers.ModelSerializer):
 
 
 class TableSerializer(serializers.ModelSerializer):
-    owner = serializers.SerializerMethodField()
+    owner = UserSerializer()
     created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Table
         fields = ['id', 'title', 'owner', 'created_at', 'share_token']
 
-    def get_owner(self, obj):
-        return obj.owner.username
     def get_created_at(self, obj):
         return int(obj.created_at.timestamp())
